f688db70423af416c809429c91c836bd7251cc25
Sharon David <89960254+sharon169961@users.noreply.github.com>
2025-04-13 11:52:10 +0530

    Create c.py

---
+++ c.py
@@ -0,0 +1,19 @@
+import random, math
+
+def random_noise():
+    random_characters = ['$', '!', 'p', '-_-', ':D', '~', '@', '#']
+    random_index = [random_characters[random.randint(0, len(random_characters) - 1)] for i in range(4)]
+
+    temp = ""
+    for i in random_index:
+        temp = temp + i
+
+    return temp
+
+
+def hypotenuse(x, b):
+    return math.sqrt(x**2 + y**2)
+
+
+if (__name__ == "__main__"):
+    print("TRY TO STOP THIS FROM BEING PRINTED IN FILE a.py WITHOUT DELETING THIS PRINT STATEMENT") #Ensuring the print statement is executed ony when the c.py program is run directly and not when it is imported
